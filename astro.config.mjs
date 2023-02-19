546841033378812dc8699ef3019fca904ca70f8f
ovcharski <48056945+ovcharski@users.noreply.github.com>
2023-02-19 19:43:13 +0200

    added matomo analytics code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import matomo from '@jop-software/astro-matomo';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,10 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    matomo({
+      baseUrl: "https://ovcharski.com/analytics/",
+      siteId: 11,
+    }),
   ],
   markdown: {
     remarkPlugins: [
